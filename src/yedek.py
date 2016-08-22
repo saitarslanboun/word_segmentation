ed62735e59ff6a213a611b4e2a68c810487afd87
sait <hasan.sait.arslan@gmail.com>
2016-08-22 18:25:56 +0300

    modification

--- src/yedek.py
+++ src/yedek.py
@@ -17,36 +17,37 @@ def generate_fsa(number_of_cluster):
 	#fsa_file_name = os.getcwd() + "/" + file_name[:-4] + ".fsa"
         text_file = open("train.fsa", "w")
 
-        text_file.write("c0\n")
+        text_file.write("0\n")
 
         dquote = '"'
-
-	#line_str = "(0 (0 *e* " + dquote + "dot" + dquote + "))\n(0 (0 *e* " + dquote + "quuestion" + dquote + "))\n"
-	#line_str = line_str + "(0 (0 *e* " + dquote + "exclamation" + dquote + "))\n(0 (0 *e* " + dquote + "tdot" + dquote + "))\n"
-	#line_str = "(c0 (c0 *e* " + dquote + "end" + dquote + "))\n"
-
-	#text_file.write(line_str)
-
-        for c in range(number_of_cluster):
-                for d in range(number_of_cluster):
-			state1 = "c" + str(c)
-			state2 = "c" + str(d)
-			equals = dquote + state2 + dquote
+        for c in range(number_of_cluster+1):
+                for d in range(number_of_cluster+1):
+                        if c == 0:
+                                state1 = "0"
+                        if c != 0: state1 = "c" + str(c)
+                        if d == 0:
+                                state2 = "0"
+                                equals = dquote + "dot" + dquote
+                        if d != 0:
+                                state2 = "c" + str(d)
+                                equals = dquote + state2 + dquote
                         line_str = "(" + state1 + " (" + state2 + " *e* " + equals + "))\n"
                         text_file.write(line_str)
+
 
         text_file.close()
 
 def generate_fst(words, number_of_cluster):
 	#fst_file_name = os.getcwd() + "/" + file_name[:-4] + ".fst"
 	text_file = open("train.fst", "w")
-	text_file.write("c0\n")
+	text_file.write("0\n")
 	dquote = '"'
-
+	line_str = "(0 (0 " + dquote + "dot" + dquote +  " " + dquote + "." + dquote + "))\n"
+	text_file.write(line_str)
+	words.remove('"."')
 	for e in range(number_of_cluster):
 		for f in range(len(words)):
-			label = "c" + str(e)
-			line_str = "(c0 (c0 " + dquote + label + dquote + " " + words[f] + "))\n"
+			line_str = "(0 (0 " + dquote + "c" + str(e + 1) + dquote + " " + words[f] + "))\n"
 			text_file.write(line_str)
 	text_file.close()
 
